4eedfef8d66c9f8149d780f31ecdb0f44b7dc4cd
Matt <spc.eubanks.matthew@gmail.com>
2018-05-28 15:26:20 -0700

    Add Breed Name at top of Picture.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function getRandomDog() {
     $.getJSON("https://dog.ceo/api/breeds/image/random", function (data) {
-        console.log(data);
         $(".image-content").html("<img src='" + data.message + "'>");
+        let url = data.message.split("/");
+        breed = url[4].toUpperCase();
+        $('#breed-name').empty();
+        displayBreed(breed);
     });
 };
 
@@ -15,17 +18,18 @@ function getChosenDog() {
     $.getJSON(`https://dog.ceo/api/breed/${dogUrl}/images/random`, function (result) {
         $('.image-content').html(`<img src=${result.message}>`);
     });
-}
+};
 
 function loadDogs() {
     $.getJSON("https://dog.ceo/api/breeds/list/all", function (result) {
         let breeds = result.message;
         let firstDog = Object.keys(breeds)[0];
+        displayBreed(firstDog);
 
         $.each(breeds, function (dog, breed) {
             if (breeds[dog].length >= 1) {
                 for (let i = 0; i < breeds[dog].length; i++) {
-                    $(".dog-selector").append(`<option value=${dog}-${breeds[dog[i]]}>${breeds[dog][i]} ${dog}</option>`);
+                    $(".dog-selector").append(`<option value=${dog}-${breeds[i]}>${breeds[dog][i]} ${dog}</option>`);
                 }
             } else if (breeds[dog].length < 1) {
                 $(".dog-selector").append(`<option value=${dog}>${dog}</option>`);
@@ -36,16 +40,18 @@ function loadDogs() {
             $('.image-content').html(`<img src=${result.message}>`);
         });
     });
-}
+};
 $('.dog-selector').change(function () {
-    $('.dog-selector option:selected').each(function () {
-        getChosenDog();
-    });
+    let breed = $('.dog-selector option:selected').val().toUpperCase();
+    displayBreed(breed);
+    getChosenDog();
 });
-$(document).ready(function () {
-    loadDogs();
-    openHidden();
-});
+
+function displayBreed(breed) {
+    $('#breed-name').empty();
+    $('#breed-name').append(`${breed}`)
+}
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -61,15 +67,22 @@ function openHidden() {
         $('.getDogPic').hide();
         $('.about').hide();
         $('.breeds').show();
+        $('#breed-name').show();
     });
     $('.randomDog').on('click', () => {
         $('.breeds').hide();
         $('.about').hide();
         $('.getDogPic').show();
+        $('#breed-name').show();
     });
     $('.aboutPage').on('click', () => {
         $('.getDogPic').hide();
         $('.breeds').hide();
+        $('#breed-name').hide();
         $('.about').show();
-    })
+    });
 };
+$(document).ready(function () {
+    loadDogs();
+    openHidden();
+});
